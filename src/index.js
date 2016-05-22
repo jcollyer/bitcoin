6e78ded04afb7848d08542a81d9534e4e2f3d679
jeremy <collyerdesign@gmail.com>
2016-05-22 16:16:05 -0400

    init

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { createStore } from 'redux'
+import Counter from './components/Counter'
+import counter from './reducers'
+
+const store = createStore(counter)
+const appElement = document.getElementById('app')
+
+function render() {
+  ReactDOM.render(
+    <Counter
+      value={store.getState()}
+      onIncrement={() => store.dispatch({ type: 'INCREMENT' })}
+      onDecrement={() => store.dispatch({ type: 'DECREMENT' })}
+    />,
+    appElement
+  )
+}
+
+render()
+store.subscribe(render)
